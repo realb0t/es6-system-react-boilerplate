e96b4c57b714923ab6410e5d1fbef66766f984a7
realbot <realbot@w3soft.ru>
2016-02-24 00:24:06 +0300

    Сделал клиенский рендер

--- src/server/server.js
+++ src/server/server.js
@@ -8,8 +8,14 @@ import routes from "../shared/routes";
 
 export const app = express();
 
+app.use('/jspm_packages', express.static('jspm_packages'));
+app.use('/config.js', express.static('config.js'));
+app.use('/client', express.static('src/client'));
+app.use('/shared', express.static('src/shared'));
+
 app.server = http.createServer(app);
 app.get('/*', function (req, res) {
+  console.log('Request server', req.url)
   match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
     if (error) {
       res.status(500).send(error.message)
--- src/shared/components/Application.js
+++ src/shared/components/Application.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 
 export default class Application extends React.Component {
   render() {
     return <div>
       <h1>Application</h1>
+      <div>
+        <Link to="/">Main</Link>
+        <span> | </span>
+        <Link to="/second">Second</Link>
+      </div>
       <div>{this.props.children}</div>
     </div>;
   }
